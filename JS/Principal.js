20b9eff53588fd73fb1d438d9f3c974c6bf13b97
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-17 17:38:57 -0500

    antiprueba2

--- JS/Principal.js
+++ JS/Principal.js
@@ -9,7 +9,7 @@ import { Escenario } from './Escenario.js'; // Importar la clase Escenario
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-//const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
 
 // Configurar el renderer para VR
 renderer.setSize(window.innerWidth, window.innerHeight);
