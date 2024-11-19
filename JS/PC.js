e27db5116c1d8dc7b78a616ec5cf0bfefe5baff3
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-19 08:22:18 -0500

    movecameraafinal

--- JS/PC.js
+++ JS/PC.js
@@ -46,7 +46,7 @@ export class PC {
                         const distance = firstHit.distance;
     
                         // Si la distancia al objeto es menor a 2, detener el movimiento
-                        if (distance < 0.5) {
+                        if (distance < 0.25) {
                             return; // No permitas movimiento adicional
                         }
                     }
