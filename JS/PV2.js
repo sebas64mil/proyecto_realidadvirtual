ed3e8f95742aba65cb6ea8ecdd8c730def908f83
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-19 15:18:48 -0500

    cambiocolor

--- JS/PV2.js
+++ JS/PV2.js
@@ -11,7 +11,7 @@ export class Extras {
         const geometry_situaciones = new THREE.PlaneGeometry( 0.5, 0.4 );
 
 
-        const material_opcion1 = new THREE.MeshBasicMaterial( {color: 0xff00, side: THREE.DoubleSide} );
+        const material_opcion1 = new THREE.MeshBasicMaterial( {color: 0xff0000, side: THREE.DoubleSide} );
         const opcion1 = new THREE.Mesh( geometry_situaciones, material_opcion1 );
         opcion1.position.set(-0.77,6.7,0)
         opcion1.rotation.y=THREE.MathUtils.degToRad(90);
