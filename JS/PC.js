4d29a5b6401b218db21371ab56655dcd643a9967
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-18 22:33:02 -0500

    move6

--- JS/PC.js
+++ JS/PC.js
@@ -13,7 +13,7 @@ export class PC {
         this.camera.rotation.y=THREE.MathUtils.degToRad(180)
         this.cameraContainer.add(this.camera); // Añadimos la cámara al contenedor
         this.scene.add(this.cameraContainer);
-        this.cameraContainer.position.set(0, 5, 0.0) // Añadimos el contenedor a la escena
+        this.cameraContainer.position.set(0, 4.5, 0.0) // Añadimos el contenedor a la escena
     }
 
     move() {
