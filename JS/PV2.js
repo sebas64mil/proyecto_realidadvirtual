1f09d137b00e41fcf2f126cc41569fef422fcb68
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2024-11-19 15:08:56 -0500

    pancartaposix

--- JS/PV2.js
+++ JS/PV2.js
@@ -13,7 +13,7 @@ export class Extras {
 
         const material_opcion1 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
         const opcion1 = new THREE.Mesh( geometry_situaciones, material_opcion1 );
-        opcion1.position.set(-0.42,8,0)
+        opcion1.position.set(-0.42,7.5,0)
         opcion1.rotation.y=THREE.MathUtils.degToRad(90);
 
         this.scene.add( opcion1 );
